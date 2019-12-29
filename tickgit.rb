46e2fb45a67a7c39b67ea6ec056aac12194a833f
augmentable <tickgit@augmentable.io>
2019-12-28 20:35:45 -0500

    Brew formula update for tickgit version v0.0.8

--- tickgit.rb
+++ tickgit.rb
@@ -2,16 +2,16 @@
 class Tickgit < Formula
   desc "Tickets as config"
   homepage "https://augmentable.io/"
-  version "0.0.7"
+  version "0.0.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/augmentable-dev/tickgit/releases/download/v0.0.7/tickgit_0.0.7_Darwin_x86_64.tar.gz"
-    sha256 "8b58a893bb6ba155c2bb2317995a1e49113b597676e54fcec9668e25ff978ce6"
+    url "https://github.com/augmentable-dev/tickgit/releases/download/v0.0.8/tickgit_0.0.8_Darwin_x86_64.tar.gz"
+    sha256 "685fff45bdb47b3ceed04b907c2538a814f507f4dbbbe9480b18b04809d6549b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/augmentable-dev/tickgit/releases/download/v0.0.7/tickgit_0.0.7_Linux_x86_64.tar.gz"
-      sha256 "a65aac3032cc7ac9b84e1c289646d3f69732dec4ca6e1d4fcb957a6625ffb108"
+      url "https://github.com/augmentable-dev/tickgit/releases/download/v0.0.8/tickgit_0.0.8_Linux_x86_64.tar.gz"
+      sha256 "9f5ed7008ac6c11241ce281558e2b80cabd1fde61291c260c52bcccc002fadc6"
     end
   end
 
